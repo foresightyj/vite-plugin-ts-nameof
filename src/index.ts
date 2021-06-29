b871ca76b8292e92f045ee8ca55c6c39495e3f85
foresightyj <imyuanjian@gmail.com>
2021-06-29 11:30:31 +0800

    Change nullish coalescing operator to ternary operator so that node with lower versions can also use this great library!

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export default (): Plugin => ({
     if ((id.includes('.ts') || id.includes('.vue')) && code.includes('nameof')) {
       const { fileText, replaced } = tsNameof.replaceInText(id, code);
       if (replaced) {
-        code = fileText ?? code;
+        code = fileText ? fileText: code;
       }
     }
 
